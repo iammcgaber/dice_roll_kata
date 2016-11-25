ef8434e14dc9c55cb540b7f35c1109c3591b9b97
Gabriel Sheeley <iammcgaber@gmail.com>
2016-11-25 14:52:19 -0500

    First classes created and added lines to .gitignore to ignore .class files.

---
+++ src/main/java/com/iammcgaber/Dice.java
@@ -0,0 +1,5 @@
+package com.iammcgaber;
+
+public class Dice {
+
+}
---
+++ src/test/java/dice_roll_kata/DiceRollTest.java
@@ -0,0 +1,29 @@
+package dice_roll_kata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.iammcgaber.Dice;
+import com.iammcgaber.DiceResults;
+
+public class DiceRollTest {
+
+	Dice dice;
+	DiceResults diceResults;
+	
+	@Before
+	public void setup() {
+		dice = new Dice();
+		diceResults = new DiceResults();
+	}
+	
+	@Test
+	public void test_objects_are_not_null() {
+		Assert.assertNotNull(dice);
+		Assert.assertNotNull(diceResults);
+	}
+
+}
